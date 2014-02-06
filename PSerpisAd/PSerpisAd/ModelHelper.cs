11a6454d5ac71dc5b21e3b9bfa2758a82963995c
dagarol <david.ies.serpis@gmail.com>
2014-02-06 11:36:13 +0100

    el load

--- PSerpisAd/PSerpisAd/ModelHelper.cs
+++ PSerpisAd/PSerpisAd/ModelHelper.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+
 
 namespace Serpis.Ad
 {
@@ -23,11 +27,40 @@ namespace Serpis.Ad
 		public static object Load(Type type, string id){
 			IDbCommand selectDbCommand = App.Instance.DbConnection.CreateCommand();
 			selectDbCommand.CommandText = GetSelect(type) + id;
-			IDataReader
+			IDataReader dataReader = selectDbCommand.ExecuteReader();
+			dataReader.Read();
+			
+			object obj = Activator.CreateInstance(type);
+			foreach (PropertyInfo propertyInfo in type.GetProperties ())
+            {
+                if (propertyInfo.IsDefined (typeof(KeyAttribute), true))
+                    propertyInfo.SetValue(obj, id, null);
+				
+                else if (propertyInfo.IsDefined (typeof(FieldAttribute), true))
+                    propertyInfo.SetValue(obj, dataReader[propertyInfo.Name.ToLower()], null); //TODO convert al tipo de destino
+            }
+			dataReader.Close ();
+            return obj;
+					
 		}
-		public static object Save (Categoria categoria){
-			//save
-		}
+		
+		public static void Save(object obj){
+           
+            IDbCommand updateDbCommand = App.Instance.DbConnection.CreateCommand();
+            Type type = obj.GetType();
+            updateDbCommand.CommandText = GetUpdate(type);
+           
+            foreach (PropertyInfo propertyInfo in type.GetProperties()){
+                if(propertyInfo.IsDefined (typeof(KeyAttribute), true)
+                    || propertyInfo.IsDefined (typeof(FieldAttribute), true)){
+                   
+                    object value = propertyInfo.GetValue(obj, null);
+                    DbCommandUtil.AddParameter(updateDbCommand, propertyInfo.Name.ToLower(), value);
+                }
+                updateDbCommand.ExecuteNonQuery();
+               
+            }
 	}
 }
-
+	
+	
